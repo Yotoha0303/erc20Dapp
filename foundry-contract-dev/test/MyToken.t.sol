7d834c141037f58ebe8d9f6daa8156fd8fe5018b
Yotoha <103411612+Yotoha0303@users.noreply.github.com>
2025-07-06 16:50:45 +0800

    new foundry test floder

---
+++ foundry-contract-dev/test/MyToken.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {MyToken} from "../contracts/MyToken.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is Test{
+    MyToken public token;
+
+    function setUp() public{
+        token = new MyToken();
+    }
+
+    function test_min() public{
+        token.mint(msg.sender,10);
+        assertEq(token.balanceOf(msg.sender), 10);
+    }
+}
