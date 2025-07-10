0f31ba61155f538164a67b5c3fd005b4e77682c7
Yotoha <103411612+Yotoha0303@users.noreply.github.com>
2025-07-10 11:50:53 +0800

    update readme

--- foundry-contract-dev/test/MyTokenRevert.t.sol
+++ foundry-contract-dev/test/MyTokenRevert.t.sol
@@ -3,18 +3,13 @@ pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 import {MyToken} from "../contracts/MyToken.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-//1、单例测试合约
-contract MyTokenTest is Test{
+//4、revert 测试合约
+//目的：验证合约是否在错误情况下正确地 revert()
+contract MyTokenRevertTest is Test{
     MyToken public token;
 
     function setUp() public{
         token = new MyToken(10000);
-    }
-
-    function test_mint() public{
-        token.mint(msg.sender,10);
-        assertEq(token.balanceOf(msg.sender), 10);
     }
 }
--- foundry-contract-dev/test/MyTokenVault.t.sol
+++ foundry-contract-dev/test/MyTokenVault.t.sol
@@ -3,18 +3,12 @@ pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 import {MyToken} from "../contracts/MyToken.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-//1、单例测试合约
-contract MyTokenTest is Test{
+//5、集成测试合约
+contract MyTokenVaultTest is Test{
     MyToken public token;
 
     function setUp() public{
         token = new MyToken(10000);
-    }
-
-    function test_mint() public{
-        token.mint(msg.sender,10);
-        assertEq(token.balanceOf(msg.sender), 10);
     }
 }
