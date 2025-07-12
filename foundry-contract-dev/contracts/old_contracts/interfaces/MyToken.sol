a54961b8bbd5f89849717b36e4b8fcaf6f6e9892
Yotoha <103411612+Yotoha0303@users.noreply.github.com>
2025-07-13 00:51:29 +0800

    delete old_contract

--- foundry-contract-dev/contracts/old_contracts/interfaces/MyToken.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface MyToken {
-
-    function mint(address to, uint256 amount) external;
-
-    function balanceOf(address account) external view returns (uint256);
-}
