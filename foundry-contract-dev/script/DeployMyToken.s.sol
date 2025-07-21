bb2a5a06813171ae2c50b3c736304ea4ace79356
Yotoha <103411612+Yotoha0303@users.noreply.github.com>
2025-07-21 19:01:22 +0800

    fix bug with deployMyToken

--- foundry-contract-dev/script/DeployMyToken.s.sol
+++ foundry-contract-dev/script/DeployMyToken.s.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.20;
 import "forge-std/Script.sol";
 import "../contracts/MyToken2.sol";
 
-contract MyToken is Script{
+contract DeployMyToken is Script{
     
-    function setUp() public{}
+    // function setUp() public{}
 
-    function run() public{
-        uint256 deployKey = vm.envString("DEPLOYER_PRIVATE_KEY");
-        vm.startBroadcast(deployKey);
+    // function run() public{
+    //     uint256 deployKey = vm.envString("DEPLOYER_PRIVATE_KEY");
+    //     vm.startBroadcast(deployKey);
 
-        MyToken2 myk = new MyToken2();
-        console.log("Deploy MyToken at :",address(myk));
+    //     MyToken2 myk = new MyToken2();
+    //     console.log("Deploy MyToken at :",address(myk));
 
-        vm.stopBroadcast();
-    }
+    //     vm.stopBroadcast();
+    // }
 }
